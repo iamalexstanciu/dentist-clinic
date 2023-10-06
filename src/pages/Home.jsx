e202050e0614b856e95abd89d9e50a7ff31995c9
iamalexstanciu <alexstanciu@outlook.it>
2023-10-06 10:46:40 +0300

    First commit.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/home.css";
+
+function Home() {
+  return (
+    <div className="section" id="home">
+      Home
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Services.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Services() {
+  return (
+    <div className="services" id="services">
+      Services
+    </div>
+  );
+}
+
+export default Services;
